cb3781ee1d84c3eff1d8d891cde689877d02043f
Oendrila Chakraborty <oendrila@itobuz.com>
2025-01-03 14:14:51 +0530

    index.js modified

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const background = document.getElementById("background-colour");
-const rgb_code=document.getElementById(
-  "random"
-)
+const rgb_code = document.getElementById("random");
 const arr = [];
-for (let i = 0; i < 256; i++) {
-  arr.push(i);
-}
+
 function colorGenerator() {
-  let first = arr[Math.floor(Math.random() * 256)];
-  let second = arr[Math.floor(Math.random() * 256)];
-  let third = arr[Math.floor(Math.random() * 256)];
-  background.style.backgroundColor = `rgb(${first},${second},${third})`;
-  rgb_code.innerText = `rgb(${first},${second},${third})`;
+  const first  =[Math.floor(Math.random() * 256)];
+  const second=[Math.floor(Math.random() * 256)];
+  const third =[Math.floor(Math.random() * 256)];
+  const randomColor = `rgb(${first},${second},${third})`
+  background.style.backgroundColor = randomColor;
+  rgb_code.innerText = randomColor;
 }
